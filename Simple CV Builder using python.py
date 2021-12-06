e0e6506187e92bded5ce4e7115b2be78b2081ec2
Chinmay Mirkute <87860979+chinmaymirkute@users.noreply.github.com>
2021-12-06 16:38:08 +0530

    Add files via upload

---
+++ Simple CV Builder using python.py
@@ -0,0 +1,37 @@
+#welcome to the very simple python CV builder
+#coded by - Chinmay Mirkute
+#you can find me on Linkedin: Chinmay Mirkute
+#                   Twitter: mirkute_chinmay
+#                   Instagram: chinmaymirkute
+print("Welcome to CV builder\n")
+#name
+name = input("What is your name?\n ")
+#address
+address = input("Enter your address\n ")
+#phone
+phone = input("Please enter your phone number\n ")
+#email
+email = input("Enter your email\n ")
+#topics interested in
+topics = input("Enter the topics you are interested in\n ")
+
+#education and Qualification
+education = input("Enter your Qualification\n ")
+#work experience
+experience = input("How much experience do you have\n ")
+
+#skills
+skills = input("What are your skills?\n ")
+#projects (If any)
+project = input("Enter some of your projects (if any)\n ")
+
+#string concatenation
+print("Here is you CV generated on the basis of your given data\n " +"NAME:\n" +name+ "\n " +"ADDRESS:\n"+address + "\n " +"PHONE:\n"+phone + "\n " +"EMAIL:\n"+email + "\n " +"TOPICS:\n"+topics + "\n " +"EDUCATION:\n"+education + "\n " +"EXPERIENCE:\n"+experience + "\n " +"SKILLS:\n"+skills + "\n " +"PROJECT:\n"+project)
+
+
+
+
+
+
+
+
