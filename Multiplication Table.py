48e0b4cee31597bf7284ff9b5559d50ddb465202
Chinmay Mirkute <87860979+chinmaymirkute@users.noreply.github.com>
2021-12-27 00:40:59 +0530

    Add files via upload

---
+++ Multiplication Table.py
@@ -0,0 +1,12 @@
+#Multiplication Table generator using python code
+#Coded by - Chinmay Mirkute
+#You can find me on Linkedin: Chinmay Mirkute
+#                   Twitter: mirkute_chinmay
+#                   Instagram: chinmaymirkute
+number = int(input("Enter the number of which you want to print the table\n"))
+num = number
+
+
+for k in range(1, 11):
+   print(num, 'x', k, '=', num*k)
+print("Thanks for using")
