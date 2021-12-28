d7b8e536869e635a265af1cad998278ecc14b2b1
Chinmay Mirkute <87860979+chinmaymirkute@users.noreply.github.com>
2021-12-28 11:39:12 +0530

    Add files via upload

---
+++ Red Floral Design.py
@@ -0,0 +1,17 @@
+#Red Abstract Design (Floral)
+#coded by - Chinmay Mirkute
+#you can find me on Linkedin: Chinmay Mirkute
+#                   Twitter: mirkute_chinmay
+#                   Instagram: chinmaymirkute
+import turtle
+
+turtle.bgcolor("black")
+Chinmay = turtle.Screen()
+Chinmay.title("Red Floral Design ")
+turtle = turtle.Turtle()
+turtle.color("red")
+turtle.speed(0)
+turtle.hideturtle()
+for i in range(200):
+    turtle.circle(i * 2)
+    turtle._rotate(256)
