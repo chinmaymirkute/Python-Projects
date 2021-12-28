a8d023294ced1609a3d46af0e5a481dd20813ff3
Chinmay Mirkute <87860979+chinmaymirkute@users.noreply.github.com>
2021-12-28 11:33:06 +0530

    Add files via upload

---
+++ Turtle_Hexagon.py
@@ -0,0 +1,27 @@
+#welcome to the Turtle_Hexagon.py
+#coded by - Chinmay Mirkute
+#you can find me on Linkedin: Chinmay Mirkute
+#                   Twitter: mirkute_chinmay
+#                   Instagram: chinmaymirkute
+
+from turtle import Turtle, Screen
+Chinmay = Turtle()
+print(Chinmay)
+Chinmay.shape("turtle")
+Chinmay.color("red")
+Chinmay.forward(90)
+Chinmay.left(300)
+Chinmay.forward(90)
+Chinmay.left(300)
+Chinmay.forward(90)
+Chinmay.left(300)
+Chinmay.forward(90)
+Chinmay.left(300)
+Chinmay.forward(90)
+Chinmay.left(300)
+Chinmay.forward(90)
+Chinmay.left(300)
+Chinmay.forward(90)
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
