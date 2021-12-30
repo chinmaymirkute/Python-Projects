11a4f9c9a24837f81469fef1f2cdd6f865627dfc
Chinmay Mirkute <87860979+chinmaymirkute@users.noreply.github.com>
2021-12-30 16:35:19 +0530

    Add files via upload

---
+++ Date Displayer.py
@@ -0,0 +1,10 @@
+#welcome to Date Displayer python code
+#coded by - Chinmay Mirkute
+#you can find me on Linkedin: Chinmay Mirkute
+#                   Twitter: mirkute_chinmay
+#                   Instagram: chinmaymirkute
+
+from datetime import date
+
+today = date.today()
+print("Today's Date is\n ", today)
