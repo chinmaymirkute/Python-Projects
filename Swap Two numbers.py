d9fc276aa9111b89d7f6450527626be200053216
Chinmay Mirkute <87860979+chinmaymirkute@users.noreply.github.com>
2021-12-30 16:58:02 +0530

    Add files via upload

---
+++ Swap Two numbers.py
@@ -0,0 +1,16 @@
+#Swap Two numbers python code
+#coded by - Chinmay Mirkute
+#you can find me on Linkedin: Chinmay Mirkute
+#                   Twitter: mirkute_chinmay
+#                   Instagram: chinmaymirkute
+
+num_1 = float(input("Enter first number :-"))
+num_2 = float(input("Enter second number :-"))
+
+
+temp = num_1
+num_1 = num_2
+num_2 = temp
+
+print('The value of first number after swapping: {}'.format(num_1))
+print('The value of second number after swapping: {}'.format(num_2))
