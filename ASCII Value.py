b4eb91c424942d975d05f609a4c9fdca8376010a
Chinmay Mirkute <87860979+chinmaymirkute@users.noreply.github.com>
2021-12-30 17:03:12 +0530

    Add files via upload

---
+++ ASCII Value.py
@@ -0,0 +1,8 @@
+#welcome to the ASCII Value generator
+#coded by - Chinmay Mirkute
+#you can find me on Linkedin: Chinmay Mirkute
+#                   Twitter: mirkute_chinmay
+#                   Instagram: chinmaymirkute
+
+chinmay = input("Enter any Alphabet:-")
+print("The ASCII value of '" + chinmay + "' is", ord(chinmay))
