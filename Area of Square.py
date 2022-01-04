93f8128ce2430fdab66b130fff95b1143a7743ec
Chinmay Mirkute <87860979+chinmaymirkute@users.noreply.github.com>
2022-01-05 00:15:24 +0530

    Add files via upload

---
+++ Area of Square.py
@@ -0,0 +1,11 @@
+#Area of Square using python code
+#Coded by - Chinmay Mirkute
+#You can find me on Linkedin: Chinmay Mirkute
+#                   Twitter: mirkute_chinmay
+#                   Instagram: chinmaymirkute
+
+
+print("Area of Square: ")
+length = float(input())
+Area = length*length
+print("Area = ", Area)
