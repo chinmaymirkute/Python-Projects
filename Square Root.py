d4e39f0ba9851bb5de5cd3b8d123654cfc281cfc
Chinmay Mirkute <87860979+chinmaymirkute@users.noreply.github.com>
2022-01-05 00:11:58 +0530

    Add files via upload

---
+++ Square Root.py
@@ -0,0 +1,12 @@
+#Square Root 
+#Coded by - Chinmay Mirkute
+#You can find me on Linkedin: Chinmay Mirkute
+#                   Twitter: mirkute_chinmay
+#                   Instagram: chinmaymirkute
+
+print("Enter a Number: ")
+num = int(input())
+
+squareroot = num ** 0.5
+
+print("Square Root =", squareroot)
