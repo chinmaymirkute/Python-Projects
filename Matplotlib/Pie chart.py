cbf4eb6574d5c5d5a2cfed34c1c8f39ebe05141b
Chinmay Mirkute <87860979+chinmaymirkute@users.noreply.github.com>
2022-01-07 22:32:33 +0530

    Update Pie chart.py

--- Matplotlib/Pie chart.py
+++ Matplotlib/Pie chart.py
@@ -11,5 +11,5 @@ import numpy as np
 computer_languages = ['Java', 'C++', 'C#', 'JavaScript', 'PHP', 'Python']
 percentage = [31.3, 9.4, 10.8, 11.3, 12.4, 24.8]
 
-plt.pie(percentage, labels = computer_languages)
+plt.pie(percentage, labels = computer_languages, autopct = '%0.1f%%')
 plt.show()
