8bb5a42e391dddc06bad7325b4d5cb706a81d373
Chinmay Mirkute <87860979+chinmaymirkute@users.noreply.github.com>
2022-01-07 22:23:18 +0530

    Add files via upload

---
+++ Matplotlib/Violin plot.py
@@ -0,0 +1,17 @@
+#Violin plot
+#Coded by - Chinmay Mirkute
+#You can find me on Linkedin: Chinmay Mirkute
+#                   Twitter: mirkute_chinmay
+#                   Instagram: chinmaymirkute
+
+from matplotlib import pyplot as plt
+import numpy as np
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+b = [3, 4, 5, 6, 7, 8, 9, 2, 1]
+c = [5, 6, 7, 8, 9, 1, 2, 3, 5, 7, 4]
+
+data = list([a, b, c])
+
+plt.violinplot(data)
+plt.show
