3eb4c08958fbd5101f1f9f74fb5b6b4299959a98
Chinmay Mirkute <87860979+chinmaymirkute@users.noreply.github.com>
2022-04-27 19:38:39 +0530

    Add files via upload

---
+++ Email Sending Using Python.py
@@ -0,0 +1,19 @@
+#Send Email using python code
+#Coded by - Chinmay Mirkute
+#You can find me on Linkedin: Chinmay Mirkute
+#                   Twitter: mirkute_chinmay
+#                   Instagram: chinmaymirkute
+
+
+import smtplib
+my_email = "your email id"
+password = ""
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="recipient email id",
+        msg="Subject:Hey there !\n\n This is a python code to send emails"
+    )
